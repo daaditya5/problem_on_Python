8e3d27a674abb60c72d7b49d5ed9c23050e9b7b0
daaditya5 <daaditya5@gmail.com>
2017-11-20 17:01:25 +0530

    edited

--- first_non_repeating.py
+++ first_non_repeating.py
@@ -1,4 +1,7 @@
-def non_rep(word):
+""" this program gives the output which is first non repeated string """
+
+def non_rep1():
+    word = input("please enter the string = ")
     list_word = list(word)
     for i in range(len(list_word)):
         vary = list_word[i]
@@ -11,9 +14,20 @@ def non_rep(word):
     else:
         print("no repeated character")
 
-while True:
-    word = input("please enter the string = ")
-    if word == 'done':
-        break
-    else:
-        non_rep(word)
+def non_rep2():
+    inp = input("please enter the string = ")
+    l = list(inp)
+    for i in range(len(l)):
+        x = l[i]
+        l[i] = 0
+        if x not in l:
+            return x
+            break
+        else:
+            l[i] = x
+
+    return None
+
+def non_rep3():
+    inp = input("please enter the string = ")
+    return [char for char in inp if inp.count(char) == 1][0]
